c6e2bd208a65017913719a4a5e89d09cfb7e74f1
estebancastano <estebin5372@gmail.com>
2024-10-23 08:59:06 -0500

    solucionado error en los enlaces vercel

--- src/components/atoms/cta-button/index.tsx
+++ src/components/atoms/cta-button/index.tsx
@@ -1,10 +1,12 @@
 import Icon from '../icon/index'
 const index = ({icon}:{icon: string}) => {
     return (
+        <a href='https://www.linkedin.com/in/estebanandresc/' target='_blank'>
         <button className="w-[154px] h-[51px] bg-textPrimary text-white flex justify-center items-center cursor-pointer rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-textSecondary duration-200">
             Contáctame
             <Icon icon={icon}/>
         </button>
+        </a>
     )
 }
 

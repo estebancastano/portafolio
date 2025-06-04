09c6777a03f399377e721fb5c8b6fd412412dc47
estebancastano <estebin5372@gmail.com>
2025-06-04 15:12:40 -0500

    Arreglado tarjetas de apartado conocimiento

--- src/components/molecules/knowledge-card/card/index.tsx
+++ src/components/molecules/knowledge-card/card/index.tsx
@@ -3,7 +3,7 @@ import { TitleTertiary, TextSecondary } from '@/components/atoms/titles'
 
 const index = ({ icon: Icon, title, text }: { icon: LucideIcon, title: string, text: string }) => {
     return (
-        <div className="bg-white flex flex-col items-center p-4 rounded-lg shadow-md w-auto h-auto group transition duration-200 hover:shadow-2xl ease-out">
+        <div className="bg-white flex flex-col items-center p-4 rounded-lg shadow-md w-auto h-auto group transition duration-200 hover:shadow-2xl ease-out cursor-default">
             <div className="mb-4 mt-4">
                 <Icon className="h-[68px] w-[68px] text-textPrimary" />
             </div>
